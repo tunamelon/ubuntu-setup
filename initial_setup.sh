b87cdd6b53f996280191be0b55f2967965e95f7f
tunamelon <108409691+tunamelon@users.noreply.github.com>
2023-09-19 21:53:48 +0100

    Update initial_setup.sh

--- initial_setup.sh
+++ initial_setup.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# Check if the script is run as root
+if [ "$EUID" -eq 0 ]; then
+    echo "It's recommended to run this script as a regular user, not as root."
+    read -p "Are you sure you want to run this as root? Type YES to continue: " confirmation
+    if [ "$confirmation" != "YES" ]; then
+        echo "Exiting..."
+        exit 1
+    fi
+fi
+
 # Update and upgrade the system
 sudo apt update && sudo apt upgrade -y
 
