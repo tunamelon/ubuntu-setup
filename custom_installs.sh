765aa67913511796c9f73f3fd2dee5c3c167e239
tunamelon <108409691+tunamelon@users.noreply.github.com>
2023-09-19 21:41:16 +0100

    Update custom_installs.sh

--- custom_installs.sh
+++ custom_installs.sh
@@ -1,1 +1,5 @@
-
+# Installing Microsoft VS Code
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+sudo apt update
+sudo apt install -y code
