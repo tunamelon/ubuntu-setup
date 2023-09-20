53902d27cc191e07a9b2262903f8d1f86e8d3c54
tunamelon <108409691+tunamelon@users.noreply.github.com>
2023-09-20 16:49:58 +0100

    Update initial_setup.sh
    Made logic in functions for easier reordering later

--- initial_setup.sh
+++ initial_setup.sh
@@ -13,6 +13,63 @@ check_status() {
     fi
 }
 
+update_upgrade_system() {
+    # Update and upgrade the system
+    echo -e "${GREEN}Updating and upgrading the system...${NC}"
+    sudo apt update && sudo apt upgrade -y
+    check_status "Failed to update and upgrade the system."
+    echo -e "${GREEN}System update and upgrade successful.${NC}"
+}
+
+install_git() {
+    # Install git
+    echo -e "${GREEN}Installing git...${NC}"
+    sudo apt install -y git
+    check_status "Failed to install git."
+    echo -e "${GREEN}Git installation successful.${NC}"
+}
+
+make_scripts_dir() {
+    # Create the directory for the setup scripts
+    echo -e "${GREEN}Setting up scripts directory...${NC}"
+    mkdir -p ~/scripts
+    check_status "Failed to create the ~/scripts directory."
+    echo -e "${GREEN}Scripts directory setup successful.${NC}"
+}
+
+clone_setup_repo() {
+    # Go to scripts
+    cd ~/scripts &&
+    # Clone the repository
+    echo -e "${GREEN}Cloning the repository...${NC}"
+    git clone https://github.com/tunamelon/ubuntu-setup.git
+    check_status "Failed to clone the repository."
+    echo -e "${GREEN}Repository clone successful.${NC}"
+
+    # Change into the cloned directory
+    echo -e "${GREEN}Changing to the ubuntu-setup directory...${NC}"
+    cd ubuntu-setup
+    check_status "Failed to change to the ubuntu-setup directory."
+    echo -e "${GREEN}Successfully changed to ubuntu-setup directory.${NC}"
+}
+
+make_scripts_executable() {    
+    # Make scripts executable
+    echo -e "${GREEN}Making scripts executable...${NC}"
+    chmod +x *.sh
+    check_status "Failed to make scripts executable."
+    echo -e "${GREEN}Scripts made executable successfully.${NC}"
+}
+
+install_software() {    
+    # Run main install script
+    echo -e "${GREEN}Running main install script...${NC}"
+    bash main_install.sh
+    check_status "Failed to run main_install.sh."
+    echo -e "${GREEN}Main install script executed successfully.${NC}"
+
+# Main logic #
+
 # Check if the script is run as root
 if [ "$EUID" -eq 0 ]; then
     echo -e "${RED}It's recommended to run this script as a regular user, not as root.${NC}"
@@ -23,44 +80,10 @@ if [ "$EUID" -eq 0 ]; then
     fi
 fi
 
-# Update and upgrade the system
-echo -e "${GREEN}Updating and upgrading the system...${NC}"
-sudo apt update && sudo apt upgrade -y
-check_status "Failed to update and upgrade the system."
-echo -e "${GREEN}System update and upgrade successful.${NC}"
-
-# Install git
-echo -e "${GREEN}Installing git...${NC}"
-sudo apt install -y git
-check_status "Failed to install git."
-echo -e "${GREEN}Git installation successful.${NC}"
-
-# Create the directory for the setup scripts and change to it
-echo -e "${GREEN}Setting up scripts directory...${NC}"
-mkdir -p ~/scripts && cd ~/scripts
-check_status "Failed to create and change to the ~/scripts directory."
-echo -e "${GREEN}Scripts directory setup successful.${NC}"
-
-# Clone the repository
-echo -e "${GREEN}Cloning the repository...${NC}"
-git clone https://github.com/tunamelon/ubuntu-setup.git
-check_status "Failed to clone the repository."
-echo -e "${GREEN}Repository clone successful.${NC}"
-
-# Change into the cloned directory
-echo -e "${GREEN}Changing to the ubuntu-setup directory...${NC}"
-cd ubuntu-setup
-check_status "Failed to change to the ubuntu-setup directory."
-echo -e "${GREEN}Successfully changed to ubuntu-setup directory.${NC}"
-
-# Make scripts executable
-echo -e "${GREEN}Making scripts executable...${NC}"
-chmod +x *.sh
-check_status "Failed to make scripts executable."
-echo -e "${GREEN}Scripts made executable successfully.${NC}"
-
-# Run main install script
-echo -e "${GREEN}Running main install script...${NC}"
-bash main_install.sh
-check_status "Failed to run main_install.sh."
-echo -e "${GREEN}Main install script executed successfully.${NC}"
+# Functions to run in this order
+update_upgrade_system
+install_git
+make_scripts_dir
+clone_setup_repo
+make_scripts_executable
+install_software
