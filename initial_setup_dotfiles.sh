4f60bf8f94d53bce5d426eebb5fb2ad59d062df7
tunamelon <108409691+tunamelon@users.noreply.github.com>
2023-09-21 16:28:18 +0100

    Update initial_setup_dotfiles.sh

--- initial_setup_dotfiles.sh
+++ initial_setup_dotfiles.sh
@@ -7,38 +7,37 @@ REPO="git@github.com:tunamelon/ubuntu-dotfiles.git"
 # Install stow
 sudo apt install -y stow
 
-# Check if dotfiles directory exists
-if [ ! -d "$DOTFILES_DIR" ]; then
-    # If not, clone the repo into the dotfiles directory
-    git clone $REPO $DOTFILES_DIR
-else
-    # If exists, pull latest changes
-    cd $DOTFILES_DIR
-    git pull
+# Navigate to or create the dotfiles directory
+mkdir -p $DOTFILES_DIR
+cd $DOTFILES_DIR
+
+# Check if git is initialized in the directory
+if [ ! -d ".git" ]; then
+    # Initialize git and add remote if it's not already initialized
+    git init
+    git remote add origin $REPO
 fi
 
-# Navigate to dotfiles directory
-cd $DOTFILES_DIR
+# Pull latest changes from the remote repo
+git pull origin main
+
+# Create necessary directories if they don't exist
+mkdir -p bash git vim vscode
 
 # Copy initial files if they exist
 [[ -f $HOME/.bashrc ]] && cp -u $HOME/.bashrc bash/
 [[ -f $HOME/.gitconfig ]] && cp -u $HOME/.gitconfig git/
 [[ -f $HOME/.vimrc ]] && cp -u $HOME/.vimrc vim/
-
-# For VS Code
-[[ -f $HOME/.config/Code/User/settings.json ]] && mkdir -p vscode && cp -u $HOME/.config/Code/User/settings.json vscode/
+[[ -f $HOME/.config/Code/User/settings.json ]] && cp -u $HOME/.config/Code/User/settings.json vscode/
 
 # Use stow to create symlinks
-[ -d "bash" ] && stow bash
-[ -d "vim" ] && stow vim
-[ -d "git" ] && stow git
-[ -d "vscode" ] && stow vscode
+stow bash vim git vscode
 
 # Git setup
 if [ -n "$(git status --porcelain)" ]; then
     git add .
     git commit -m "Update dotfiles"
-    git push
+    git push -u origin main
 else
     echo "No changes to commit"
 fi
