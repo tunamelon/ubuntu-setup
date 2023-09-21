8b158cff54a434cdbf21b0d6e07348e8f282db44
tunamelon <108409691+tunamelon@users.noreply.github.com>
2023-09-21 15:50:18 +0100

    Create initial_setup_dotfiles.sh

---
+++ initial_setup_dotfiles.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Variables
+DOTFILES_DIR="$HOME/dotfiles"
+REPO="https://github.com/tunamelon/ubuntu-dotfiles"
+
+# Install stow
+sudo apt install -y stow
+
+# Create the dotfiles directory and cd into it
+mkdir -p $DOTFILES_DIR
+cd $DOTFILES_DIR
+
+# Clone the repo into the dotfiles directory
+git clone $REPO .
+
+# Copy initial files
+cp $HOME/.bashrc bash/
+cp $HOME/.gitconfig git/
+cp $HOME/.vimrc vim/
+
+# For VS Code
+mkdir -p vscode
+cp $HOME/.config/Code/User/settings.json vscode/
+
+# Use stow to create symlinks
+stow bash vscode git vim
+
+# Git setup
+git add .
+git commit -m "Initial commit of dotfiles"
+git push
+
+echo "Dotfiles setup and pushed to GitHub."
