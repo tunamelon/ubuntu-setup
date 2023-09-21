9f428be3abceff8dd5558136544c35f1757bc297
tunamelon <108409691+tunamelon@users.noreply.github.com>
2023-09-21 16:32:22 +0100

    Update initial_setup_dotfiles.sh

--- initial_setup_dotfiles.sh
+++ initial_setup_dotfiles.sh
@@ -7,22 +7,22 @@ REPO="git@github.com:tunamelon/ubuntu-dotfiles.git"
 # Install stow
 sudo apt install -y stow
 
-# Navigate to or create the dotfiles directory
+# Set up the dotfiles directory and navigate to it
 mkdir -p $DOTFILES_DIR
 cd $DOTFILES_DIR
 
 # Check if git is initialized in the directory
 if [ ! -d ".git" ]; then
-    # Initialize git and add remote if it's not already initialized
+    # Initialize git, add remote, and create the main branch
     git init
     git remote add origin $REPO
+    git checkout -b main
 fi
 
-# Pull latest changes from the remote repo
-git pull origin main
-
-# Create necessary directories if they don't exist
-mkdir -p bash git vim vscode
+# Create the necessary directories, only if they don't already exist
+for dir in bash git vim vscode; do
+    mkdir -p $dir
+done
 
 # Copy initial files if they exist
 [[ -f $HOME/.bashrc ]] && cp -u $HOME/.bashrc bash/
@@ -34,12 +34,8 @@ mkdir -p bash git vim vscode
 stow bash vim git vscode
 
 # Git setup
-if [ -n "$(git status --porcelain)" ]; then
-    git add .
-    git commit -m "Update dotfiles"
-    git push -u origin main
-else
-    echo "No changes to commit"
-fi
+git add .
+git commit -m "Initial commit of dotfiles"
+git push -u origin main
 
 echo "Dotfiles setup and pushed to GitHub."
