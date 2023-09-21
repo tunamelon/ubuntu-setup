aa611db816597467460d2c501dcb5451ecd77086
tunamelon <108409691+tunamelon@users.noreply.github.com>
2023-09-21 15:46:08 +0100

    Create restore_dotfiles.sh

---
+++ restore_dotfiles.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Variables
+DOTFILES_DIR="$HOME/dotfiles"
+REPO="https://github.com/tunamelon/ubuntu-dotfiles"
+
+# Clone the repository
+if [[ ! -d $DOTFILES_DIR ]]; then
+    mkdir -p $DOTFILES_DIR
+    git clone $REPO $DOTFILES_DIR
+else
+    echo "$DOTFILES_DIR already exists. Pulling the latest changes..."
+    cd $DOTFILES_DIR
+    git pull origin master
+fi
+
+# Navigate to the dotfiles directory
+cd $DOTFILES_DIR
+
+# Use stow to symlink the files
+stow bash vscode git vim
+
+echo "Restoration complete."
