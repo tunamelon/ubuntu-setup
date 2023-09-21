03b7a1a36df70fddcbd295caff2e46b21c7b7c0c
tunamelon <108409691+tunamelon@users.noreply.github.com>
2023-09-21 15:47:54 +0100

    Update backup_dotfiles.sh

--- backup_dotfiles.sh
+++ backup_dotfiles.sh
@@ -3,8 +3,6 @@
 # Variables
 DOTFILES_DIR="$HOME/dotfiles"
 REPO="https://github.com/tunamelon/ubuntu-dotfiles"
-PROFILE_DIR=$(grep 'Path=' ~/.mozilla/firefox/profiles.ini | sed 's/^Path=//')
-BOOKMARKS_BACKUP="$DOTFILES_DIR/firefox/firefox_bookmarks.json"
 
 # Check if the dotfiles directory exists
 if [ ! -d "$DOTFILES_DIR" ]; then
@@ -24,20 +22,9 @@ cp $HOME/.vimrc vim/
 # VS Code
 cp $HOME/.config/Code/User/settings.json vscode/
 
-# Firefox
-# Export Firefox bookmarks
-firefox -headless -CreateProfile "backup-temp"
-firefox -headless -P "backup-temp" bookmarkbackups/export > $BOOKMARKS_BACKUP
-# Backup prefs.js
-cp ~/.mozilla/firefox/$PROFILE_DIR/prefs.js firefox/
-
 # Commit and push changes to the repository
 git add .
 git commit -m "Backup dotfiles on $(date +'%Y-%m-%d %H:%M:%S')"
 git push origin master
-
-# Remove temporary Firefox profile
-firefox -headless -P "backup-temp" -quit
-rm -r ~/.mozilla/firefox/*.backup-temp
 
 echo "Dotfiles backed up and pushed to GitHub."
